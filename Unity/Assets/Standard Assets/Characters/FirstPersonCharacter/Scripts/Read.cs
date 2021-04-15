bea68734d89f1b684ab63f5b207947e4c27c4d2e
Arttu-Lakkala <60192672+Arttu-Lakkala@users.noreply.github.com>
2021-04-16 00:39:46 +0300

    Added word creation

--- Unity/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Read.cs
+++ Unity/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Read.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class Read : MonoBehaviour
 {
-    public TextAsset textFile;
+    public TextAsset wordFile;
+    public TextAsset letterFile;
     private string[] wordlist;
+    private string[] letterlist;
     // Start is called before the first frame update
     void Start()
     {
-       wordlist = textFile.text.Split('\n');
-       Debug.Log(wordlist.Length);
+       wordlist = wordFile.text.Split('\n');
+       letterlist = letterFile.text.Split('\n');
     }
 
     // Update is called once per frame
     void Update()
     {
+       //rightclick
        if(Input.GetMouseButtonUp(1))
        {
          RaycastHit hit;
@@ -23,8 +26,62 @@ public class Read : MonoBehaviour
          
          if(Physics.Raycast(ray, out hit, 1000))
          {
-           Debug.Log(hit.collider.gameObject.name);
+           Debug.Log(wordlist[Random.Range(0,wordlist.Length)]);
          }
-       }         
+       }
+       //Leftclick
+       if(Input.GetMouseButtonUp(0))
+       {
+         int precentage = Random.Range(1,100);
+         int wordLength;
+         string word = "";
+         //get a random precentage and convert to word length
+         if(precentage<5)
+         {
+          wordLength = 1;
+         }
+         else if(precentage<15)
+         {
+          wordLength = 2;
+         }
+         else if(precentage<25)
+         {
+          wordLength = 3;
+         }
+         else if(precentage<45)
+         {
+          wordLength = 4;
+         }
+         else if(precentage<65)
+         {
+          wordLength = 5;
+         }
+         else if(precentage<75)
+         {
+          wordLength = 6;
+         }
+         else if(precentage<85)
+         {
+          wordLength = 7;
+         }
+         else if(precentage<90)
+         {
+          wordLength = 8;
+         }
+         else if(precentage<95)
+         {
+          wordLength = 9;
+         }
+         else
+         {
+          wordLength = 10;
+         }
+         //generate word based on length
+         for(int i=0; i<wordLength; i++)
+         {
+           word = word + letterlist[Random.Range(0,25)];
+         }
+         Debug.Log(word);
+       }       
     }
 }
